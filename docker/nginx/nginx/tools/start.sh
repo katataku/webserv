c06715472d1355ebcbff6841ddc9645243fa1ec0
katataku <katataku.dev@gmail.com>
2022-07-02 17:27:47 +0900

    feat/CGIの引数とメソッドに応じて処理を追加

--- docker/nginx/nginx/tools/start.sh
+++ docker/nginx/nginx/tools/start.sh
@@ -1,5 +1,5 @@
 #------------------------ boot database  ----------------------------------------
-chmod 777 /var/www/localhost/cgi-bin/test.py
+chmod 777 /var/www/localhost/cgi-bin/file_manager.py
 mkdir /var/www/localhost/files
 chmod 777 /var/www/localhost/files
 service fcgiwrap start
