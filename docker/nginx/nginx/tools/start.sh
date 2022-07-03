3f644bf510c63d161cfd22ba449c369d274b2df2
katataku <katataku.dev@gmail.com>
2022-07-03 17:31:56 +0900

    fix/nginxコンテナ用のリソースをtest_data配下に移動

--- docker/nginx/nginx/tools/start.sh
+++ docker/nginx/nginx/tools/start.sh
@@ -1,4 +1,10 @@
 #------------------------ boot database  ----------------------------------------
+cp /app/tests/test_data/conf/localhost /etc/nginx/sites-available
+ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled
+
+cp /app/tests/test_data/public/html/upload.html /var/www/localhost
+cp /app/tests/test_data/public/cgi-bin/file_manager.py /var/www/localhost/cgi-bin 
+
 chmod 777 /var/www/localhost/cgi-bin/file_manager.py
 mkdir /var/www/localhost/files
 chmod 777 /var/www/localhost/files
