df97ce2d594910072399bbcaaef69557fee63f8b
kataoka takumi <katataku.dev@gmail.com>
2022-07-03 20:34:00 +0900

    CGI関連のモジュールをdockerディレクトリの外に出す。 (#13)
    * fix/google testディレクトリ変更
    
    * fix/nginxコンテナ用のリソースをtest_data配下に移動
    
    * fix/nginxコンテナ内のディレクトリ構成を整理
    
    * fix ディレクトリ構成変更
    
    * fix googletestの階層戻した

--- docker/nginx/nginx/tools/start.sh
+++ docker/nginx/nginx/tools/start.sh
@@ -1,7 +1,13 @@
 #------------------------ boot database  ----------------------------------------
-chmod 777 /var/www/localhost/cgi-bin/file_manager.py
-mkdir /var/www/localhost/files
-chmod 777 /var/www/localhost/files
+cp /app/test_data/conf/localhost /etc/nginx/sites-available
+ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled
+
+cp /app/sample_data/html/upload.html /var/www/html/
+cp /app/sample_data/cgi-bin/file_manager.py /var/www/cgi-bin/
+
+chmod 777 /var/www/cgi-bin/file_manager.py
+mkdir /var/www/files
+chmod 777 /var/www/files
 service fcgiwrap start
 echo "Starting nginx";
 nginx -g "daemon off;";
