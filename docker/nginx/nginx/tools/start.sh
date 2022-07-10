c3f8db1892ebbb3832f7d5bd9e7bdc3a3b2deaf4
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-07-10 21:47:19 +0900

    add: CGI Request Dumper script

--- docker/nginx/nginx/tools/start.sh
+++ docker/nginx/nginx/tools/start.sh
@@ -3,9 +3,11 @@ cp /app/test_data/config/nginx/localhost /etc/nginx/sites-available
 ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled
 
 cp /app/sample_data/html/upload.html /var/www/html/
+cp /app/sample_data/html/index.html /var/www/html/
 cp /app/sample_data/cgi-bin/file_manager.py /var/www/cgi-bin/
+cp /app/sample_data/cgi-bin/cgi-req-dumper.py /var/www/cgi-bin/
 
-chmod 777 /var/www/cgi-bin/file_manager.py
+chmod -R 777 /var/www/cgi-bin/
 mkdir /var/www/files
 chmod 777 /var/www/files
 service fcgiwrap start
