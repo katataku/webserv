ee25360923c0c226cd94a7c9a6accc8ffa05f764
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-07-11 13:22:58 +0900

    fix: can work cgi_tester

--- docker/nginx/nginx/tools/start.sh
+++ docker/nginx/nginx/tools/start.sh
@@ -2,10 +2,11 @@
 cp /app/test_data/config/nginx/localhost /etc/nginx/sites-available
 ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled
 
-cp /app/sample_data/html/upload.html /var/www/html/
-cp /app/sample_data/html/index.html /var/www/html/
-cp /app/sample_data/cgi-bin/file_manager.py /var/www/cgi-bin/
-cp /app/sample_data/cgi-bin/cgi-req-dumper.py /var/www/cgi-bin/
+cp /app/sample_data/html/* /var/www/html/
+cp /app/sample_data/cgi-bin/* /var/www/cgi-bin/
+
+mkdir -p /var/www/images/
+cp /app/sample_data/images/* /var/www/images
 
 chmod -R 777 /var/www/cgi-bin/
 mkdir /var/www/files
