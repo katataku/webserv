c650649a2c54cd20847117f93fc1863354781bb4
Yamada_Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-07-12 10:39:05 +0900

    CGIリクエストをダンプしHTMLとして表示するCGIスクリプトの追加 (#46)
    * add: CGI Request Dumper script
    
    * fix: rename index.html
    
    * fix: can work cgi_tester

--- docker/nginx/nginx/tools/start.sh
+++ docker/nginx/nginx/tools/start.sh
@@ -2,10 +2,13 @@
 cp /app/test_data/config/nginx/localhost /etc/nginx/sites-available
 ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled
 
-cp /app/sample_data/html/upload.html /var/www/html/
-cp /app/sample_data/cgi-bin/file_manager.py /var/www/cgi-bin/
+cp /app/sample_data/html/* /var/www/html/
+cp /app/sample_data/cgi-bin/* /var/www/cgi-bin/
 
-chmod 777 /var/www/cgi-bin/file_manager.py
+mkdir -p /var/www/images/
+cp /app/sample_data/images/* /var/www/images
+
+chmod -R 777 /var/www/cgi-bin/
 mkdir /var/www/files
 chmod 777 /var/www/files
 service fcgiwrap start
