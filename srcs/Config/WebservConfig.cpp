a22d915bb6c376811afb9d7f5e007533b8fff06b
Ayato Hayashi <go12limchangyong@gmail.com>
2022-07-15 07:53:22 +0700

    ダミーのconfigを用意 (#53)
    * feat: ServerLocationにメンバ変数とゲッターを用意
    
    * fix: CGI周りの処理を追加&Getter以外のpublicな関数を追加
    
    * feat: CreateServerLocationsでダミーデータを作成するように
    
    * fix: cpp lintの修正
    
    * fix: commit漏れ
    
    * fix: markdownのformatを修正
    
    * fix: path_info -> path
    
    * fix: vector->set
    
    * fix: cpplint対応
    
    * Update docs/class/config_class.md
    
    Co-authored-by: Yamada_Ika <73375669+Yamada-Ika@users.noreply.github.com>
    
    Co-authored-by: Yamada_Ika <73375669+Yamada-Ika@users.noreply.github.com>

--- srcs/Config/WebservConfig.cpp
+++ srcs/Config/WebservConfig.cpp
@@ -13,6 +13,23 @@ WebservConfig &WebservConfig::operator=(WebservConfig const &other) {
 
 WebservConfig::~WebservConfig() {}
 WebservConfig *WebservConfig::Parse() { return new WebservConfig(); }
+
+static ServerLocation CreateServerLocation() {
+    std::map<int, std::string> error_pages;
+    error_pages[404] = "/404.html";
+    error_pages[500] = "/50x.html";
+    error_pages[501] = "/50x.html";
+    error_pages[505] = "/50x.html";
+    std::set<std::string> allow_methods;
+    allow_methods.insert("GET");
+    allow_methods.insert("POST");
+    allow_methods.insert("DELETE");
+    return ServerLocation(8081, "webserv1", "/html", error_pages, 4086, false,
+                          "index.html", "", allow_methods, "/var/www", "");
+}
+
 std::vector<ServerLocation> *WebservConfig::CreateServerLocations() {
-    return new std::vector<ServerLocation>();
+    std::vector<ServerLocation> *vec = new std::vector<ServerLocation>();
+    vec->push_back(CreateServerLocation());
+    return vec;
 }
--- srcs/Webserv/ServerLocation.cpp
+++ srcs/Webserv/ServerLocation.cpp
@@ -12,3 +12,57 @@ ServerLocation &ServerLocation::operator=(ServerLocation const &other) {
 }
 
 ServerLocation::~ServerLocation() {}
+
+bool ServerLocation::IsRedirect() const { return !this->redirect_uri_.empty(); }
+
+// TODO(ahayashi): 実装する。utilsに移してもいいかもしれない。
+static std::string GetExtension(std::string path) {
+    (void)path;
+    return "py";
+}
+
+bool ServerLocation::IsCGI(std::string path) const {
+    if (this->cgi_extension_.empty()) {
+        return false;
+    }
+    return GetExtension(path) == this->cgi_extension_;
+}
+
+int ServerLocation::port() const { return port_; }
+const std::string &ServerLocation::host() const { return host_; }
+const std::string &ServerLocation::path() const { return path_; }
+const std::map<int, std::string> &ServerLocation::error_pages() const {
+    return error_pages_;
+}
+int ServerLocation::client_max_body_size() const {
+    return client_max_body_size_;
+}
+bool ServerLocation::auto_index() const { return auto_index_; }
+const std::string &ServerLocation::index_page() const { return index_page_; }
+const std::string &ServerLocation::redirect_uri() const {
+    return redirect_uri_;
+}
+const std::set<std::string> &ServerLocation::allow_methods() const {
+    return allow_methods_;
+}
+const std::string &ServerLocation::alias() const { return alias_; }
+const std::string &ServerLocation::cgi_extension() const {
+    return cgi_extension_;
+}
+ServerLocation::ServerLocation(
+    int port, const std::string &host, const std::string &path,
+    const std::map<int, std::string> &error_pages, int client_max_body_size,
+    bool auto_index, const std::string &index_page,
+    const std::string &redirect_uri, const std::set<std::string> &allow_methods,
+    const std::string &alias, const std::string &cgi_extension)
+    : port_(port),
+      host_(host),
+      path_(path),
+      error_pages_(error_pages),
+      client_max_body_size_(client_max_body_size),
+      auto_index_(auto_index),
+      index_page_(index_page),
+      redirect_uri_(redirect_uri),
+      allow_methods_(allow_methods),
+      alias_(alias),
+      cgi_extension_(cgi_extension) {}
