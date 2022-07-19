5c4fd9ad99b41b4d3ec2ffc625cb9c666f78a5b4
katataku <katataku.dev@gmail.com>
2022-07-19 13:52:47 +0900

    fix/request

--- test_data/request/GET_simple.sh
+++ test_data/request/GET_simple.sh
@@ -1,3 +1,3 @@
 # --silent : Silent or quiet mode. Don't show progress meter or error messages.
 # --include: Include the HTTP response headers in the output. 
-curl --silent --include http://localhost:8081/for_review.html
+curl --silent --include http://localhost:8080/for_review.html
