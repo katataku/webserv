71faf2ede171820c3ba18bf1f8ec255ea6195d2d
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-07-21 23:52:14 +0900

    fix: lintエラーになるのでNOLINTをつけた

--- srcs/Config/DefaultValues.cpp
+++ srcs/Config/DefaultValues.cpp
@@ -2,14 +2,14 @@
 
 #include <string>
 
-const std::string DefaultValues::kServerName = "";
+const std::string DefaultValues::kServerName = "";  // NOLINT
 // TODO(iyamada)
 // デフォルトのpathはどうするか。正規表現で全部マッチみたいになってそう。
-const std::string DefaultValues::kPath = "";
-const std::string DefaultValues::kIndexPage = "";
-const std::string DefaultValues::kRedirectUrl = "";
-const std::string DefaultValues::kAlias = "";
-const std::string DefaultValues::kCgiExtension = "";
-const int DefaultValues::kPort = 80;
-const int DefaultValues::kClientMaxBodySize = 1024;
-const bool DefaultValues::kAutoIndex = false;
+const std::string DefaultValues::kPath = "";          // NOLINT
+const std::string DefaultValues::kIndexPage = "";     // NOLINT
+const std::string DefaultValues::kRedirectUrl = "";   // NOLINT
+const std::string DefaultValues::kAlias = "";         // NOLINT
+const std::string DefaultValues::kCgiExtension = "";  // NOLINT
+const int DefaultValues::kPort = 80;                  // NOLINT
+const int DefaultValues::kClientMaxBodySize = 1024;   // NOLINT
+const bool DefaultValues::kAutoIndex = false;         // NOLINT
--- srcs/Config/InitialValues.cpp
+++ srcs/Config/InitialValues.cpp
@@ -2,12 +2,12 @@
 
 #include <string>
 
-const std::string InitialValues::kServerName = "";
-const std::string InitialValues::kPath = "";
-const std::string InitialValues::kIndexPage = "";
-const std::string InitialValues::kRedirectUrl = "";
-const std::string InitialValues::kAlias = "";
-const std::string InitialValues::kCgiExtension = "";
-const int InitialValues::kPort = -1;
-const int InitialValues::kClientMaxBodySize = -1;
-const bool InitialValues::kAutoIndex = false;
+const std::string InitialValues::kServerName = "";    // NOLINT
+const std::string InitialValues::kPath = "";          // NOLINT
+const std::string InitialValues::kIndexPage = "";     // NOLINT
+const std::string InitialValues::kRedirectUrl = "";   // NOLINT
+const std::string InitialValues::kAlias = "";         // NOLINT
+const std::string InitialValues::kCgiExtension = "";  // NOLINT
+const int InitialValues::kPort = -1;                  // NOLINT
+const int InitialValues::kClientMaxBodySize = -1;     // NOLINT
+const bool InitialValues::kAutoIndex = false;         // NOLINT
