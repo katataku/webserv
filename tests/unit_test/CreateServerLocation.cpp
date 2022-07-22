85be02d29b2ad3172cbf85a0a62c07f962dc9354
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-07-22 14:09:06 +0900

    fix: googleテストの1ケース をdisableに

--- tests/unit_test/CreateServerLocation.cpp
+++ tests/unit_test/CreateServerLocation.cpp
@@ -172,80 +172,88 @@ TEST_F(CreateServerLocationTest, two_location_contexts) {
     ASSERT_EQ(locate_sl2.alias(), "/var/www/hoge");
 }
 
+class DISABLED_CreateServerLocationTest : public ::testing::Test {
+ protected:
+    static void SetUpTestCase() {}
+    static void TearDownTestCase() {}
+    virtual void SetUp() {}
+    virtual void TearDown() {}
+};
+
 /*
     autoindexをboolで持っているため、autoindex
    offが設定されているか、設定されていないかが判断できない。だから浅いコンテキストの値で上書きされてしまう
 */
-// TEST_F(CreateServerLocationTest, complex_test) {
-//     /* 想定したconfigファイル
-//         autoindex on;
-//         server {
-//             listen 8080;
-//             location / {
-//                 alias /var/www/html;
-//             }
-//             location /hoge {
-//                 alias /var/www/hoge;
-//             }
-//         }
-//         server {
-//             listen 81;
-//             autoindex off;
-//         }
-//     */
-//     WebservConfig conf;
-//     ServerContext serv_contx1;
-//     ServerContext serv_contx2;
-//     LocationContext locate_contx1;
-//     LocationContext locate_contx2;
+TEST_F(DISABLED_CreateServerLocationTest, complex_test) {
+    /* 想定したconfigファイル
+        autoindex on;
+        server {
+            listen 8080;
+            location / {
+                alias /var/www/html;
+            }
+            location /hoge {
+                alias /var/www/hoge;
+            }
+        }
+        server {
+            listen 81;
+            autoindex off;
+        }
+    */
+    WebservConfig conf;
+    ServerContext serv_contx1;
+    ServerContext serv_contx2;
+    LocationContext locate_contx1;
+    LocationContext locate_contx2;
 
-//     locate_contx1.set_path("/");
-//     locate_contx1.set_alias("/var/www/html");
-//     locate_contx2.set_path("/hoge");
-//     locate_contx2.set_alias("/var/www/hoge");
+    locate_contx1.set_path("/");
+    locate_contx1.set_alias("/var/www/html");
+    locate_contx2.set_path("/hoge");
+    locate_contx2.set_alias("/var/www/hoge");
 
-//     serv_contx1.set_port(8080);
-//     serv_contx1.PushLocationContext(locate_contx1);
-//     serv_contx1.PushLocationContext(locate_contx2);
-//     serv_contx2.set_port(81);
-//     serv_contx2.set_auto_index(false);
+    serv_contx1.set_port(8080);
+    serv_contx1.PushLocationContext(locate_contx1);
+    serv_contx1.PushLocationContext(locate_contx2);
+    serv_contx2.set_port(81);
+    serv_contx2.set_auto_index(false);
 
-//     conf.PushServerContext(serv_contx1);
-//     conf.PushServerContext(serv_contx2);
-//     conf.set_auto_index(true);
+    conf.PushServerContext(serv_contx1);
+    conf.PushServerContext(serv_contx2);
+    conf.set_auto_index(true);
 
-//     std::map<ServerLocationKey, ServerLocation> sl_container =
-//         conf.CreateServerLocations();
+    std::map<ServerLocationKey, ServerLocation> sl_container =
+        conf.CreateServerLocations();
 
-//     ASSERT_EQ(sl_container.size(), 4);
+    ASSERT_EQ(sl_container.size(), 4);
 
-//     // serverコンテキストのServerLocationをチェック
-//     ServerLocationKey serv_key1("8080", "", "");
-//     ServerLocation serv_sl1 = sl_container[serv_key1];
+    // serverコンテキストのServerLocationをチェック
+    ServerLocationKey serv_key1("8080", "", "");
+    ServerLocation serv_sl1 = sl_container[serv_key1];
 
-//     ASSERT_EQ(serv_sl1.port(), 8080);
-//     ASSERT_EQ(serv_sl1.auto_index(), true);
+    ASSERT_EQ(serv_sl1.port(), 8080);
+    ASSERT_EQ(serv_sl1.auto_index(), true);
 
-//     ServerLocationKey serv_key2("81", "", "");
-//     ServerLocation serv_sl2 = sl_container[serv_key2];
+    ServerLocationKey serv_key2("81", "", "");
+    ServerLocation serv_sl2 = sl_container[serv_key2];
 
-//     ASSERT_EQ(serv_sl2.port(), 81);
-//     ASSERT_EQ(serv_sl2.auto_index(), false);
+    ASSERT_EQ(serv_sl2.port(), 81);
+    ASSERT_EQ(serv_sl2.auto_index(), false);
 
-//     // locationコンテキストのServerLocationをチェック
-//     ServerLocationKey locate_key1("8080", "", "/");
-//     ServerLocation locate_sl1 = sl_container[locate_key1];
+    // locationコンテキストのServerLocationをチェック
+    ServerLocationKey locate_key1("8080", "", "/");
+    ServerLocation locate_sl1 = sl_container[locate_key1];
 
-//     ASSERT_EQ(locate_sl1.port(), 8080);
-//     ASSERT_EQ(locate_sl1.auto_index(), true);
-//     ASSERT_EQ(locate_sl1.path(), "/");
-//     ASSERT_EQ(locate_sl1.alias(), "/var/www/html");
+    ASSERT_EQ(locate_sl1.port(), 8080);
+    ASSERT_EQ(locate_sl1.auto_index(), true);
+    ASSERT_EQ(locate_sl1.path(), "/");
+    ASSERT_EQ(locate_sl1.alias(), "/var/www/html");
 
-//     ServerLocationKey locate_key2("8080", "", "/hoge");
-//     ServerLocation locate_sl2 = sl_container[locate_key2];
+    ServerLocationKey locate_key2("8080", "", "/hoge");
+    ServerLocation locate_sl2 = sl_container[locate_key2];
 
-//     ASSERT_EQ(locate_sl2.port(), 8080);
-//     ASSERT_EQ(locate_sl2.auto_index(), true);
-//     ASSERT_EQ(locate_sl2.path(), "/hoge");
-//     ASSERT_EQ(locate_sl2.alias(), "/var/www/hoge");
-// }
+    ASSERT_EQ(locate_sl2.port(), 8080);
+    ASSERT_EQ(locate_sl2.auto_index(), true);
+    ASSERT_EQ(locate_sl2.path(), "/hoge");
+    ASSERT_EQ(locate_sl2.alias(), "/var/www/hoge");
+}
