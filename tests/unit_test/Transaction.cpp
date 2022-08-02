765d72286ca8e568a62633d0ea7cac898fd02223
hayashi-ay <go12limchangyong@gmail.com>
2022-08-02 15:50:26 +0900

    fix: 単体テストの修正

--- tests/unit_test/Transaction.cpp
+++ tests/unit_test/Transaction.cpp
@@ -78,27 +78,8 @@ TEST_F(TransactionTest, Allowed_methods_single) {
 
     Transaction tr;
     HTTPResponse *res = tr.Exec(&req, &sl);
-    ASSERT_EQ(res->status_code(), 403);
+    ASSERT_EQ(res->status_code(), 405);
     ASSERT_EQ(res->allow(), "GET");
-}
-
-TEST_F(TransactionTest, Allowed_methods) {
-    HTTPRequest req = HTTPRequest();
-    req.set_method("HOGE");
-
-    std::map<int, std::string> error_pages;
-    std::set<std::string> allow_methods;
-    allow_methods.insert("GET");
-    allow_methods.insert("POST");
-    allow_methods.insert("DELETE");
-    ServerLocation sl =
-        ServerLocation(8081, "webserv1", "/html", error_pages, 4086, "off",
-                       "index.html", "", allow_methods, "/var/www", "");
-
-    Transaction tr;
-    HTTPResponse *res = tr.Exec(&req, &sl);
-    ASSERT_EQ(res->status_code(), 403);
-    ASSERT_EQ(res->allow(), "DELETE, GET, POST");
 }
 
 TEST_F(TransactionTest, client_max_body_size) {
