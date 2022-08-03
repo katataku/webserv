630748a711a2f83595a69e57a502142c885c4da7
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-08-04 01:26:03 +0900

    fix: 上の方に移動

--- srcs/Socket/IOMultiplexer.cpp
+++ srcs/Socket/IOMultiplexer.cpp
@@ -51,6 +51,21 @@ void IOMultiplexer::CreateEpollInstance() {
     }
 }
 
+void IOMultiplexer::CreateListenerSocket(const std::string &port) {
+    Socket *new_socket = Socket::OpenListeningSocket(port);
+    this->sockets_.push_back(new_socket);
+    this->fd_port_map_.insert(
+        std::pair<int, std::string>(new_socket->sock_fd(), new_socket->port()));
+}
+
+void IOMultiplexer::CreateListenerSockets(
+    const std::vector<std::string> &ports) {
+    for (std::vector<std::string>::const_iterator itr = ports.begin();
+         itr != ports.end(); ++itr) {
+        this->CreateListenerSocket(*itr);
+    }
+}
+
 void IOMultiplexer::Init(std::vector<std::string> ports) {
     this->logging_.Debug("Init start");
 
@@ -120,21 +135,6 @@ void IOMultiplexer::Accept(Socket const &socket) {
         std::pair<int, std::string>(conn_fd, socket.port()));
 
     this->logging_.Debug("Accept");
-}
-
-void IOMultiplexer::CreateListenerSocket(const std::string &port) {
-    Socket *new_socket = Socket::OpenListeningSocket(port);
-    this->sockets_.push_back(new_socket);
-    this->fd_port_map_.insert(
-        std::pair<int, std::string>(new_socket->sock_fd(), new_socket->port()));
-}
-
-void IOMultiplexer::CreateListenerSockets(
-    const std::vector<std::string> &ports) {
-    for (std::vector<std::string>::const_iterator itr = ports.begin();
-         itr != ports.end(); ++itr) {
-        this->CreateListenerSocket(*itr);
-    }
 }
 
 void IOMultiplexer::CloseFd(int fd) {
