efc5276fb53fbaa7429584262f3f74edadb2201a
katataku <katataku.dev@gmail.com>
2022-08-04 17:30:04 +0900

    feat/args

--- srcs/Webserv/Webserv.cpp
+++ srcs/Webserv/Webserv.cpp
@@ -24,10 +24,12 @@ Webserv::~Webserv() {}
 void Webserv::Run(int argc, char **argv) {
     this->logging_.Debug(argv[0]);
     WebservConfig config;
-    // TODO(takkatao): 引数の数が多い時のエラー処理を追加。
     if (argc == 2) {
         this->logging_.Debug("config : " + std::string(argv[1]));
         config = WebservConfig::Parse(argv[1]);
+    } else if (argc > 2) {
+        this->logging_.Fatal("too many args");
+        exit(1);
     } else {
         this->logging_.Debug("config : default");
         config = WebservConfig::Parse();
