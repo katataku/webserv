b20ce8b4beb47ea0952bd7fdf9ac082d74c147fa
hayashi-ay <go12limchangyong@gmail.com>
2022-08-04 17:29:46 +0900

    fix: alias, indexのデフォルト値を設定

--- srcs/Config/DefaultValues.cpp
+++ srcs/Config/DefaultValues.cpp
@@ -2,13 +2,12 @@
 
 #include <string>
 
-const std::string DefaultValues::kServerName = "";   // NOLINT
-const std::string DefaultValues::kPath = "";         // NOLINT
-const std::string DefaultValues::kIndexPage = "";    // NOLINT
-const std::string DefaultValues::kRedirectUrl = "";  // NOLINT
-// TODO(hayashi-ay): aliasのデフォルト値を設定する
-const std::string DefaultValues::kAlias = "";         // NOLINT
-const std::string DefaultValues::kCgiExtension = "";  // NOLINT
+const std::string DefaultValues::kServerName = "";               // NOLINT
+const std::string DefaultValues::kPath = "";                     // NOLINT
+const std::string DefaultValues::kIndexPage = "index.html";      // NOLINT
+const std::string DefaultValues::kRedirectUrl = "";              // NOLINT
+const std::string DefaultValues::kAlias = "./sample_data/html";  // NOLINT
+const std::string DefaultValues::kCgiExtension = "";             // NOLINT
 const int DefaultValues::kPort = 80;
 const int DefaultValues::kClientMaxBodySize = 1024;
 const std::string DefaultValues::kAutoIndex = "off";  // NOLINT
