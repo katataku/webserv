52f5034ba5658737258de7d11e7b6bb5d58bb269
Yamada_Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-08-05 17:58:16 +0900

    fix: コマンドライン引数のディレクトリチェックとリファクタ (#206)

--- srcs/Webserv/Webserv.cpp
+++ srcs/Webserv/Webserv.cpp
@@ -1,6 +1,9 @@
 #include "Webserv.hpp"
 
+#include <sys/stat.h>
+
 #include <cstdlib>
+#include <cstring>
 #include <string>
 #include <vector>
 
@@ -8,6 +11,7 @@
 #include "ServerLocationFacade.hpp"
 #include "SuperVisor.hpp"
 #include "WebservConfig.hpp"
+#include "utils.hpp"
 
 Webserv::Webserv() : logging_(Logging(__FUNCTION__)) {}
 
@@ -22,19 +26,43 @@ Webserv &Webserv::operator=(Webserv const &other) {
 
 Webserv::~Webserv() {}
 
+static bool IsDir(const std::string &path) {
+    struct stat st;
+
+    if (stat(path.c_str(), &st) == -1) {
+        throw std::runtime_error("Error: stat " + std::string(strerror(errno)));
+    }
+    return S_ISDIR(st.st_mode);
+}
+
+static std::string ParseArg(int argc, char **argv) {
+    if (argc == 1) {
+        return "./default.conf";
+    }
+    if (argc != 2) {
+        throw std::runtime_error("Error: Too many arguments");
+    }
+
+    if (IsDir(argv[1])) {
+        throw std::runtime_error("Error: " + std::string(argv[1]) +
+                                 " is directory");
+    }
+    return argv[1];
+}
+
 void Webserv::Run(int argc, char **argv) {
     this->logging_.Debug(argv[0]);
     WebservConfig config;
-    if (argc == 2) {
-        this->logging_.Debug("config : " + std::string(argv[1]));
-        config = WebservConfig::Parse(argv[1]);
-    } else if (argc > 2) {
-        this->logging_.Fatal("too many args");
-        exit(1);
-    } else {
-        this->logging_.Debug("config : default");
-        config = WebservConfig::Parse("./default.conf");
+
+    try {
+        std::string conf_path = ParseArg(argc, argv);
+        this->logging_.Debug("config : " + conf_path);
+        config = WebservConfig::Parse(conf_path);
+    } catch (std::exception &e) {
+        this->logging_.Fatal(e.what());
+        std::exit(1);
     }
+
     std::vector<ServerLocation> locations = config.CreateServerLocations();
     ServerLocationFacade facade(locations);
     SuperVisor sv(&facade);
