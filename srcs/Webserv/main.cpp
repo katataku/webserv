c0e0218090340e71c4adbd21b5e08e3e5acc03ae
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-08-08 16:57:58 +0900

    fix: エラーメッセージを修正

--- srcs/Webserv/main.cpp
+++ srcs/Webserv/main.cpp
@@ -7,7 +7,8 @@ static void InstallIgnoreSignalHandler() {
     // ゾンビプロセスを回収するためにSIGCHLDをignore
     if (sigaction(SIGPIPE, &ignore_act, NULL) == -1 ||
         sigaction(SIGCHLD, &ignore_act, NULL) == -1) {
-        throw std::runtime_error(strerror(errno));
+        throw std::runtime_error("Error: sigaction " +
+                                 std::string(strerror(errno)));
     }
 }
 
