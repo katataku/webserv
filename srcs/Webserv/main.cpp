3c5499650d32077bdb59fa10a2908c3dc287fd6d
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-08-08 16:52:23 +0900

    fix: コメントを追加

--- srcs/Webserv/main.cpp
+++ srcs/Webserv/main.cpp
@@ -4,6 +4,7 @@ static void InstallIgnoreSignalHandler() {
     struct sigaction ignore_act;
     bzero(&ignore_act, sizeof(struct sigaction));
     ignore_act.sa_handler = SIG_IGN;
+    // ゾンビプロセスを回収するためにSIGCHLDをignore
     if (sigaction(SIGPIPE, &ignore_act, NULL) == -1 ||
         sigaction(SIGCHLD, &ignore_act, NULL) == -1) {
         throw std::runtime_error(strerror(errno));
